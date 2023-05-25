374bdf994f1a02f7103d96a9d752d1aad1954825
oem <guyhaim55@gmail.com>
2023-05-25 18:45:20 +0300

    commit shuvaut

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+
 
 typedef struct {
     int sports;
@@ -11,8 +14,18 @@ typedef struct {
     int queue_size;
     int product_num;
     char **queue;
+    int front;  // Index of the front element in the queue
+    int rear;   // Index of the rear element in the queue
+    // Semaphore for mutual exclusion
+    sem_t mutex;
+    // Semaphore for signaling available items in the queue,so who want to remove an item from the queue will
+    // wait (sem_wait) on the items semaphore until it becomes available
+    sem_t items;
+    // Semaphore for signaling available slots in the queue
+    sem_t slots;
 } Producer;
 
+int producerCount = 0;
 Producer *producers = NULL;
 
 const char *getProductType(int randomNumber) {
@@ -28,6 +41,37 @@ const char *getProductType(int randomNumber) {
     }
 }
 
+void insertItem(Producer *producer, char *item) {
+    sem_wait(&producer->slots);  // Wait for an available slot in the queue
+    sem_wait(&producer->mutex);  // Acquire the mutex to access the queue
+
+    // Insert the item into the queue
+    producer->queue[producer->rear] = item;
+    producer->rear = (producer->rear + 1) % producer->queue_size;
+
+    sem_post(&producer->mutex);  // Release the mutex
+    sem_post(&producer->items);  // Signal that an item has been inserted
+    //printf("Item inserted: %s\n", item);  // Print the inserted item
+
+}
+
+char *removeItem(Producer *producer) {
+    char *item;
+
+    sem_wait(&producer->items);  // Wait for an item in the queue
+    sem_wait(&producer->mutex);  // Acquire the mutex to access the queue
+
+    // Remove the item from the queue
+    item = producer->queue[producer->front];
+    producer->front = (producer->front + 1) % producer->queue_size;
+
+    sem_post(&producer->mutex);  // Release the mutex
+    sem_post(&producer->slots);  // Signal that a slot is available
+    //printf("Item removed: %s\n", item);  // Print the removed item
+
+    return item;
+}
+
 
 int countDigitsOfNum(int num) {
     if (num == 0) {
@@ -41,35 +85,8 @@ int countDigitsOfNum(int num) {
     return count;
 }
 
-void updateProductCount(Producer *producer, int randomNumber,char **s) {
-    switch (randomNumber) {
-        case 0:
-            *s = malloc(strlen("producer ") + countDigitsOfNum(producer->id) + strlen(" ") + strlen("SPORTS") +
-                       strlen(" ") + countDigitsOfNum(producer->sports) +
-                       strlen("\0")); // Allocate memory for the string
-            sprintf(*s, "Producer %d %s %d", producer->id, getProductType(randomNumber), producer->sports);
-            producer->sports++;
-            break;
-        case 1:
-            *s = malloc(strlen("producer ") + countDigitsOfNum(producer->id) + strlen(" ") + strlen("NEWS") +
-                       strlen(" ") + countDigitsOfNum(producer->news) + strlen("\0")); // Allocate memory for the string
-            sprintf(*s, "Producer %d %s %d", producer->id, getProductType(randomNumber), producer->news);
-            producer->news++;
-            break;
-        case 2:
-            *s = malloc(strlen("producer ") + countDigitsOfNum(producer->id) + strlen(" ") + strlen("WEATHER") +
-                       strlen(" ") + countDigitsOfNum(producer->weather) +
-                       strlen("\0")); // Allocate memory for the string
-            sprintf(*s, "Producer %d %s %d", producer->id, getProductType(randomNumber), producer->weather);
-            producer->weather++;
-            break;
-        default:
-            break;
-    }
-}
 
-
-Producer createProducer(int producer_id, int product_num, int queue_size, int count) {
+Producer createProducer(int producer_id, int product_num, int queue_size) {
     Producer producer;
     producer.id = producer_id;
     producer.product_num = product_num;
@@ -77,7 +94,12 @@ Producer createProducer(int producer_id, int product_num, int queue_size, int co
     producer.sports = 0;
     producer.news = 0;
     producer.weather = 0;
-    producers = realloc(producers, (count + 1) * sizeof(Producer));
+    producer.front = 0;
+    producer.rear = 0;
+    sem_init(&producer.mutex, 0, 1);    // Initialize mutex semaphore to 1 (available)
+    sem_init(&producer.items, 0, 0);    // Initialize items semaphore to 0 (unavailable)
+    sem_init(&producer.slots, 0, queue_size);   // Initialize slots semaphore to the queue size
+    producers = realloc(producers, (producerCount + 1) * sizeof(Producer));
     if (producers == NULL) {
         printf("Memory allocation failed.\n");
         //TODO return error
@@ -87,34 +109,69 @@ Producer createProducer(int producer_id, int product_num, int queue_size, int co
         printf("Memory allocation failed.\n");
         //TODO return error
     }
-    // Generate a random number between 0 and 2
-    char *productArray[producer.product_num];
-    char *s= NULL; ;
-    int counter = 0;
-    for (int i = 0; i < producer.product_num; ++i) {
-        int randomNumber = rand() % 3;
-        updateProductCount(&producer, randomNumber,&s);
-        productArray[i]=s;
-        counter++;
-        printf("%s\n",s);
-    }
     return producer;
 }
 
 int readConfFile(FILE *file) {
-    int producer_args, argOne, argTwo, argThree, count = 0;
+    int producer_args, argOne, argTwo, argThree;
     while ((producer_args = fscanf(file, "%d\n%d\n%d\n\n", &argOne, &argTwo, &argThree)) && producer_args == 3) {
-        Producer producer = createProducer(argOne, argTwo, argThree, count);
-        producers[count] = producer;
-        count++;
+        Producer producer = createProducer(argOne, argTwo, argThree);
+        producers[producerCount] = producer;
+        producerCount++;
     }
-    for (int i = 0; i < count; i++) {
+    int coEditorCapacity = argOne;
+    for (int i = 0; i < producerCount; i++) {
         printf("Producer ID: %d\n", producers[i].id);
         printf("Queue Size: %d\n", producers[i].queue_size);
         printf("-------------\n");
     }
+    return coEditorCapacity;
 }
 
+
+int createProduct(Producer producer) {
+    char *s = NULL;
+    for (int i = 0; i < producer.product_num; i++) {
+        // Generate a random number between 0 and 2
+        int randomNumber = rand() % 3;
+        switch (randomNumber) {
+            case 0:
+                s = malloc(strlen("producer ") + countDigitsOfNum(producer.id) + strlen(" ") + strlen("SPORTS") +
+                           strlen(" ") + countDigitsOfNum(producer.sports) +
+                           1); // Allocate memory for the string
+                sprintf(s, "Producer %d %s %d", producer.id, getProductType(randomNumber), producer.sports);
+                producer.sports++;
+                break;
+            case 1:
+                s = malloc(strlen("producer ") + countDigitsOfNum(producer.id) + strlen(" ") + strlen("NEWS") +
+                           strlen(" ") + countDigitsOfNum(producer.news) +
+                           1); // Allocate memory for the string
+                sprintf(s, "Producer %d %s %d", producer.id, getProductType(randomNumber), producer.news);
+                producer.news++;
+                break;
+            case 2:
+                s = malloc(strlen("producer ") + countDigitsOfNum(producer.id) + strlen(" ") + strlen("WEATHER") +
+                           strlen(" ") + countDigitsOfNum(producer.weather) +
+                           1); // Allocate memory for the string
+                sprintf(s, "Producer %d %s %d", producer.id, getProductType(randomNumber), producer.weather);
+                producer.weather++;
+                break;
+            default:
+                break;
+        }
+        insertItem(&producer, s);  // Insert the item into the queue
+        printf("%s\n", s);
+         removeItem(&producer);
+    }
+}
+
+
+void runThreads_Producers(pthread_t threads[]) {
+    for (int i = 0; i < producerCount; i++) {
+        createProduct(producers[i]);
+    }        //pthread_create(&threads[i], NULL, (void *)createProduct, &producers[i]);
+
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -128,6 +185,16 @@ int main(int argc, char *argv[]) {
     }
     // Read the file line by line
     srand(time(NULL));
-    readConfFile(file);
+
+    int coEditorCapacity = readConfFile(file);
+
+
+    //create threads by number of producer + 3 co-Editor + dispatcher
+  pthread_t threads[producerCount + 4];
+    runThreads_Producers(threads);
+
+    for (int i = 0; i < producerCount + 4; i++) {
+        //pthread_join(threads[i], NULL);
+    }
     return 0;
 }
